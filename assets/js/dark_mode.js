9b13f7bbcc2b50b1bc74791f925fe751cd248095
sauravjoshi23 <sauravjoshi2362000@gmail.com>
2024-02-12 20:28:34 -0800

    test

--- assets/js/dark_mode.js
+++ assets/js/dark_mode.js
@@ -1,8 +1,6 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var mode_toggle = document.getElementById("light-toggle");
-
-    mode_toggle.addEventListener("click", function() {
-        toggleTheme(localStorage.getItem("theme"));
-    });
+document.addEventListener('DOMContentLoaded', function () {
+  var mode_toggle = document.getElementById("light-toggle");
+  mode_toggle.addEventListener("click", function () {
+    toggleTheme(localStorage.getItem("theme"));
+  });
 });
-
